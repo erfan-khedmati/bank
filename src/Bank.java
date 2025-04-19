c3375bb46d4cf8f78ab5cc5bc6ca82c32e3a7715
Erfan Khedmati <100248103+EranKhedmati@users.noreply.github.com>
2025-04-19 14:13:28 +0330

    initialize bank class

---
+++ src/Bank.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class Bank {
+    final private String username = "admin";
+    final private String password = "admin";
+
+    private ArrayList<User> users;
+
+}
