017c821e6fca25187ac35db5a960de953ddde485
Erfan Khedmati <100248103+EranKhedmati@users.noreply.github.com>
2025-04-19 14:27:33 +0330

    add main panel

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,40 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Scanner scanner = new Scanner(System.in);
+
+        // handle panel
+        int choice = 0;
+
+        while (choice == 0) {
+            System.out.println("""
+                1) Bank Admin
+                2) Sign in
+                3) Sign up
+                """);
+            System.out.print("Enter your choice: ");
+            choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.println("Your account has been created!");
+                    break;
+                case 2:
+                    System.out.println("Your account has been updated!");
+                    break;
+                case 3:
+                    System.out.println("Your account has been deleted!");
+                    break;
+                default:
+                    System.out.println("\nInvalid choice!\n");
+                    choice = 0;
+            }
+        }
+
+
+
+
+        scanner.close();
     }
 }
---
+++ src/handelers/Signup.java
@@ -0,0 +1,5 @@
+package handelers;
+
+public class Signup {
+
+}
